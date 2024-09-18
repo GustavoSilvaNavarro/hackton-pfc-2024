0b0c3950b53d30146d0844d1d3d8faeaa147ba2a
Javier Nicolás Ledesma <nicolas.ledesma@powerflex.com>
2024-09-18 15:35:18 -0500

    Change acg, acc, acg and acs by the pfid.

--- app/server/routes/route.py
+++ app/server/routes/route.py
@@ -4,10 +4,11 @@ router = APIRouter()
 
 
 @router.get(
-    "/constraint/{acn_id}/acc/{acc_id}", tags=["Constraint"], description="Find a site that it is under constraint", status_code=status.HTTP_200_OK
+    "/constraint/{pfid}", tags=["Constraint"], description="Find an acs that it is under constraint or not", status_code=status.HTTP_200_OK
 )
-async def find_constraint(acn_id: str, acc_id: str):
+async def find_constraint(pfid: str):
     """It should find a site that is in constraint."""
-    query = await find_constrain_data(acn=acn_id, acc=acc_id)
+    acn_id, acc_id, acg_id, acs_id = pfid.split("-")
+    query = await find_constrain_data(acn=acn_id, acc=acc_id, acg=acg_id, acs=acs_id)
     print(query)
     return {"msg": "Hello"}
